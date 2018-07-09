ea8d944cc6947239a7a9bbce49f97d52b0b6a2db
Rajesh Markandan <Rajesh.Markandan@blackboard.com>
2018-07-09 19:02:33 +0530

    commented the error lines on Xls_Reader.java

--- src/test/java/com/excel/utility/Xls_Reader.java
+++ src/test/java/com/excel/utility/Xls_Reader.java
@@ -287,9 +287,9 @@ public Xls_Reader(String path) {
 		    hlink_style.setFont(hlink_font);
 		    //hlink_style.setWrapText(true);
 
-		    XSSFHyperlink link = createHelper.createHyperlink(XSSFHyperlink.LINK_FILE);
-		    link.setAddress(url);
-		    cell.setHyperlink(link);
+//		    XSSFHyperlink link = createHelper.createHyperlink(XSSFHyperlink.LINK_FILE);
+//		    link.setAddress(url);
+//		    cell.setHyperlink(link);
 		    cell.setCellStyle(hlink_style);
 		      
 		    fileOut = new FileOutputStream(path);
@@ -353,8 +353,8 @@ public Xls_Reader(String path) {
 				
 			XSSFCellStyle style = workbook.createCellStyle();
 			style.setFillForegroundColor(HSSFColor.GREY_40_PERCENT.index);
-			style.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
-			
+//			style.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
+//			
 			sheet=workbook.getSheetAt(index);
 			
 			row = sheet.getRow(0);
@@ -396,7 +396,7 @@ public Xls_Reader(String path) {
 			XSSFCellStyle style = workbook.createCellStyle();
 			style.setFillForegroundColor(HSSFColor.GREY_40_PERCENT.index);
 			XSSFCreationHelper createHelper = workbook.getCreationHelper();
-			style.setFillPattern(HSSFCellStyle.NO_FILL);
+//			style.setFillPattern(HSSFCellStyle.NO_FILL);
 			
 		    
 		
